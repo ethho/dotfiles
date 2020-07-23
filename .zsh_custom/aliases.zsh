92419f7505fc2c798ed1f218e4acbec860c49f46
Ethan Ho <ethan.n.ho@gmail.com>
2020-07-23 09:30:13 -0600

    Trim down catalina zsh config for HPC

---
+++ .zsh_custom/aliases.zsh
@@ -0,0 +1,8 @@
+# **************************** zsh aliases *************************************
+
+alias ll="ls -lah"
+alias t="tapis"
+
+# git track dotfiles
+alias config="git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+
