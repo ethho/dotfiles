fd43ea4ba5435bb0b9d9ea48941ffdc5818f63c5
Ethan Ho <eho@tacc.utexas.edu>
2020-08-04 17:05:51 -0500

    Add a sourced file for zsh envs

---
+++ zsh_custom/envs.zsh
@@ -0,0 +1,5 @@
+# Map common Tapis storage systems to their realpaths
+export TAPIS_SD2E_COMMUNITY=/work/projects/SD2E-Community/prod/data
+export TAPIS_P11=/work/projects/SD2E-Community/prod/projects/sd2e-project-11
+export HADDOXW=/work/05402/haddox
+export VER_DF_PD=/work/projects/SD2E-Community/prod/data/versioned-dataframes/protein-design/
