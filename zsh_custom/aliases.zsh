2f4d47075b5568ebe72723176fba569570be56de
Ethan Ho <eho@tacc.utexas.edu>
2021-06-07 23:28:29 -0500

    Pacman shortcuts

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -6,3 +6,7 @@ alias bfg="java -jar ~/.local/bin/bfg-1.13.0.jar"
 alias dk="docker"
 alias dkc="docker-compose"
 alias c="code -r"
+
+# pacman
+alias install="sudo pacman -S"
+alias update="sudo pacman -Suyy"
