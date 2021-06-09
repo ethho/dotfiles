55b4ad9b03ffd83abf4110af82aa28195ff89b17
Ethan Ho <eho@tacc.utexas.edu>
2021-06-09 17:28:02 -0500

    All signs point to nvim

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -1,5 +1,7 @@
 # **************************** zsh aliases *************************************
 
+alias vi="nvim"
+alias vim="nvim"
 alias ll="ls -lah"
 alias tp="tapis"
 alias bfg="java -jar ~/.local/bin/bfg-1.13.0.jar"
