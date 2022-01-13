91e8b1f675cb75c3a21b8eed672db539f2ab7f73
Ethan Ho <eho@tacc.utexas.edu>
2022-01-12 20:33:33 -0700

    Restore pyautogui implementation of jitter.py

--- bin/jitter.py
+++ bin/jitter.py
@@ -1,39 +1,15 @@
 #!/usr/bin/python3
 
-from subprocess import call
-
-
-def set_suspend(val: bool = True, mode: str = 'ac'):
-    """
-    gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
-    gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'suspend'
-    gsettings set org.gnome.desktop.session idle-delay 'uint32 0'
-    gsettings set org.gnome.settings-daemon.plugins.power idle-brightness 30
-    """
-    assert mode in ('battery', 'ac')
-    if val is True:
-        setting = "suspend"
-        idle_delay = '300'
-        idle_brightness = '30'
-    else:
-        setting = "nothing"
-        idle_delay = '0'
-        idle_brightness = '1'
-    _ = call(["gsettings", "set", "org.gnome.settings-daemon.plugins.power", f"sleep-inactive-{mode}-type", f"'{setting}'"])
-    _ = call(["gsettings", "set", "org.gnome.desktop.session", "idle-delay", f"uint32 {idle_delay}"])
-    _ = call(["gsettings", "set", "org.gnome.settings-daemon.plugins.power", "idle-brightness", idle_brightness])
+import pyautogui as pag
 
 
 def main():
     try:
-        set_suspend(False, 'ac')
         while True:
-            pass
+            pag.moveRel(20, 20, 3)
+            pag.moveRel(-20, -20, 3)
     except KeyboardInterrupt:
         pass
-    finally:
-        set_suspend(True, 'ac')
-
 
 
 if __name__ == "__main__":
