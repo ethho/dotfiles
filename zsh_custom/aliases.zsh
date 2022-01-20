43172562f341f2e0f380e6b89a546c2379e2a1f0
Ethan Ho <eho@tacc.utexas.edu>
2022-01-19 17:06:56 -0700

    Useful aliases

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -1,7 +1,6 @@
-# **************************** zsh aliases *************************************
-
 alias ll="ls -lah"
 alias tp="tapis"
+alias t="task"
 alias bfg="java -jar ~/.local/bin/bfg-1.13.0.jar"
 alias dk="docker"
 alias dkc="docker-compose"
@@ -9,6 +8,17 @@ alias c="code -r"
 alias vim="nvim"
 alias open="xdg-open"
 
-# pacman
-alias install="yay -S"
-alias update="yay -Suyy"
+# apt
+alias install="sudo apt install"
+alias update="sudo apt update && sudo apt upgrade"
+alias remove="sudo autoremove"
+
+md2pdf() {
+  pandoc $1 -o `basename $1 .md`.pdf
+}
+
+
+search() {
+  apt search $1 | rg $1
+}
+
