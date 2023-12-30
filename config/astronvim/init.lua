29ae1fa73fabeebd73b8ad7b6f2bb46d97150679
Ethan Ho <ethan.n.ho@gmail.com>
2023-12-30 09:46:30 -0700

    Messing with astronvim themes

--- config/astronvim/init.lua
+++ config/astronvim/init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "gruvbox-material",
+  colorscheme = "oxocarbon",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
