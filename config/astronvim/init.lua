c3eb7c3e95642f3cc84fc219a66c241f2cfe4a13
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-01-14 08:49:09 -0700

    gruvbox in nvim

--- config/astronvim/init.lua
+++ config/astronvim/init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "oxocarbon",
+  colorscheme = "gruvbox-material",
+  -- colorscheme = "oxocarbon",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
