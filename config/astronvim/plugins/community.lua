7f6ea1acd7835c118c8bb76657d7c083f92b9163
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-01-18 19:37:04 -0700

    astronvim tweaks

--- config/astronvim/plugins/community.lua
+++ config/astronvim/plugins/community.lua
@@ -25,4 +25,17 @@ return {
       disabled_filetypes = { "help" },
     },
   },
+  -- {
+  --   "akinsho/toggleterm.nvim",
+  --   opts = {
+  --     size = 10,
+  --     open_mapping = "[[<C-,>]]",
+  --     shading_factor = 2,
+  --     direction = "float",
+  --     float_opts = {
+  --       border = "curved",
+  --       highlights = { border = "Normal", background = "Normal" },
+  --     },
+  --   }
+  -- },
 }
